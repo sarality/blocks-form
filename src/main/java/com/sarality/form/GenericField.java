eb49d2e9fa64518fe744b6c7ec9202ccfff2e36c
Abhideep Singh <abhi@dothat.in>
2016-10-11 18:40:21 +0530

    Add Name for Form Field

--- src/main/java/com/sarality/form/GenericField.java
+++ src/main/java/com/sarality/form/GenericField.java
@@ -9,19 +9,28 @@ import java.util.List;
  * @author abhideep@ (Abhideep Singh)
  */
 public class GenericField implements FormField {
+
+  private final String name;
   private final int viewId;
   private final List<Integer> childViewIdList = new ArrayList<>();
   private final ControlType controlType;
 
-  public GenericField(int viewId, ControlType controlType) {
-    this.viewId = viewId;
-    this.controlType = controlType;
+  public GenericField(String name, int viewId, ControlType controlType) {
+    this(name, viewId, controlType, null);
   }
 
-  public GenericField(int viewId, List<Integer> childViewIdList, ControlType controlType) {
+  public GenericField(String name, int viewId, ControlType controlType, List<Integer> childViewIdList) {
+    this.name = name;
     this.viewId = viewId;
-    this.childViewIdList.addAll(childViewIdList);
     this.controlType = controlType;
+    if (childViewIdList != null) {
+      this.childViewIdList.addAll(childViewIdList);
+    }
+  }
+
+  @Override
+  public String getName() {
+    return name;
   }
 
   @Override
