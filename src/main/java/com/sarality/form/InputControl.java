6c397696f1a0b6539e23e3b8b347548337c3da2a
Abhideep Singh <abhi@dothat.in>
2018-09-20 10:03:14 +0530

    Register Readers for the Controls

--- src/main/java/com/sarality/form/InputControl.java
+++ src/main/java/com/sarality/form/InputControl.java
@@ -10,6 +10,11 @@ import com.sarality.form.binding.TagCloudBinding;
 import com.sarality.form.binding.TimePickerBinding;
 import com.sarality.form.binding.ViewBindingFactory;
 import com.sarality.form.binding.ViewTagBinding;
+import com.sarality.form.reader.CompoundListReader;
+import com.sarality.form.reader.ControlReaderFactory;
+import com.sarality.form.reader.DropDownReader;
+import com.sarality.form.reader.EditTextReader;
+import com.sarality.form.reader.ViewTagReader;
 
 /**
  * Enumeration for the Type of Field provided by the Android System.
@@ -17,20 +22,23 @@ import com.sarality.form.binding.ViewTagBinding;
  * @author abhideep@ (Abhideep Singh)
  */
 public enum InputControl implements ControlType {
-  CHECK_BOX(new CheckBoxBinding.Factory()),
-  CHECK_BOX_GROUP(new CheckBoxGroupBinding.Factory()),
-  DATE_PICKER(new DatePickerBinding.Factory()),
-  DROP_DOWN(new DropDownBinding.Factory()),
-  EDIT_TEXT(new EditTextBinding.Factory()),
-  RADIO_BUTTON_GROUP(new RadioButtonGroupBinding.Factory()),
-  TIME_PICKER(new TimePickerBinding.Factory()),
-  TAG_CLOUD(new TagCloudBinding.Factory()),
-  VIEW_TAG(new ViewTagBinding.Factory());
+  CHECK_BOX(new CheckBoxBinding.Factory(), null),
+  CHECK_BOX_GROUP(new CheckBoxGroupBinding.Factory(), null),
+  DATE_PICKER(new DatePickerBinding.Factory(), null),
+  DROP_DOWN(new DropDownBinding.Factory(), new DropDownReader.Factory()),
+  EDIT_TEXT(new EditTextBinding.Factory(), new EditTextReader.Factory()),
+  COMPOUND_LIST(null, new CompoundListReader.Factory()),
+  RADIO_BUTTON_GROUP(new RadioButtonGroupBinding.Factory(), null),
+  TIME_PICKER(new TimePickerBinding.Factory(), null),
+  TAG_CLOUD(new TagCloudBinding.Factory(), null),
+  VIEW_TAG(new ViewTagBinding.Factory(), new ViewTagReader.Factory());
 
   private final ViewBindingFactory bindingFactory;
+  private final ControlReaderFactory readerFactory;
 
-  InputControl(ViewBindingFactory bindingFactory) {
+  InputControl(ViewBindingFactory bindingFactory, ControlReaderFactory readerFactory) {
     this.bindingFactory = bindingFactory;
+    this.readerFactory = readerFactory;
   }
 
   @Override
@@ -41,5 +49,10 @@ public enum InputControl implements ControlType {
   @Override
   public ViewBindingFactory getBindingFactory() {
     return bindingFactory;
+  }
+
+  @Override
+  public ControlReaderFactory getReaderFactory() {
+    return readerFactory;
   }
 }
