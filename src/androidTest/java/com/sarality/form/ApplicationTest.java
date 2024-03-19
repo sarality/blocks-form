c7039570b56d1fd8e97f18a34d47eb13fb4a661e
Abhideep Singh <abhideep@>
2024-03-19 15:47:42 +0530

    Target SDK Version 33

--- src/androidTest/java/com/sarality/form/ApplicationTest.java
+++ src/androidTest/java/com/sarality/form/ApplicationTest.java
@@ -1,13 +1,22 @@
 package com.sarality.form;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
+import static junit.framework.TestCase.assertEquals;
 
-/**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
- */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-  public ApplicationTest() {
-    super(Application.class);
+import android.content.Context;
+
+import androidx.test.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class ApplicationTest {
+  @Test
+  public void useAppContext() throws Exception {
+    // Context of the app under test.
+    Context appContext = InstrumentationRegistry.getTargetContext();
+
+    assertEquals("com.sarality.form.test", appContext.getPackageName());
   }
 }
